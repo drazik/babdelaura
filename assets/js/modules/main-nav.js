120f83acc03724bcba6aa6916fc870a379d56dc4
Cyrille Perois <cperois@outlook.com>
2014-11-28 20:30:09 +0100

    Suppression de info-cookies et jquery.cookies

--- assets/js/modules/main-nav.js
+++ assets/js/modules/main-nav.js
@@ -1,29 +1,26 @@
-var jQuery = require('jquery');
+'use strict';
 
-(function($) {
+var $ = require('jquery');
 
-    'use strict';
+function MainNav() {
+    this.$mainNav = $('#main-nav');
+    this.$mainNavToggleButton = this.$mainNav.find('#main-nav-toggle');
+    this.$mainNavToggleIcon = this.$mainNavToggleButton.find('.fa');
+    this.$mainNavContainer = this.$mainNav.find('#main-nav-container');
+}
 
-    function MainNav() {
-        this.$mainNav = $('#main-nav');
-        this.$mainNavToggleButton = this.$mainNav.find('#main-nav-toggle');
-        this.$mainNavToggleIcon = this.$mainNavToggleButton.find('.fa');
-        this.$mainNavContainer = this.$mainNav.find('#main-nav-container');
-    }
+MainNav.prototype.initEvents = function() {
+    this.$mainNavToggleButton.on('click', this.toggleNavigation.bind(this));
+};
 
-    MainNav.prototype.initEvents = function() {
-        this.$mainNavToggleButton.on('click', this.toggleNavigation.bind(this));
-    };
+MainNav.prototype.toggleNavigation = function() {
+    this.$mainNav.toggleClass('open');
+    this.$mainNavToggleIcon.toggleClass('fa-bars');
+    this.$mainNavToggleIcon.toggleClass('fa-times');
+};
 
-    MainNav.prototype.toggleNavigation = function() {
-        this.$mainNav.toggleClass('open');
-        this.$mainNavToggleIcon.toggleClass('fa-bars');
-        this.$mainNavToggleIcon.toggleClass('fa-times');
-    };
+MainNav.prototype.run = function() {
+    this.initEvents();
+};
 
-    MainNav.prototype.run = function() {
-        this.initEvents();
-    };
-
-    module.exports = MainNav;
-})(jQuery);
+module.exports = MainNav;
--- assets/js/modules/toucheffect.js
+++ assets/js/modules/toucheffect.js
@@ -1,30 +1,27 @@
-var jQuery = require('jquery');
+'use strict';
 
-(function($) {
+var $ = require('jquery');
 
-    'use strict';
+function TouchEffect() {
+    this.$articles = $('.grid-item-article');
+}
 
-    function TouchEffect() {
-        this.$articles = $('.grid-item-article');
+TouchEffect.prototype.initEvents = function() {
+    this.$articles.on('click', '.grid-item-article-read', function(event) {
+        event.stopPropagation();
+    });
+
+    this.$articles.on('click', this.toggleArticle);
+};
+
+TouchEffect.prototype.toggleArticle = function() {
+    $(this).toggleClass('hover');
+};
+
+TouchEffect.prototype.run = function() {
+    if (this.$articles.length > 0 && Modernizr.touch) {
+        this.initEvents();
     }
+};
 
-    TouchEffect.prototype.initEvents = function() {
-        this.$articles.on('click', '.grid-item-article-read', function(event) {
-            event.stopPropagation();
-        });
-
-        this.$articles.on('click', this.toggleArticle);
-    };
-
-    TouchEffect.prototype.toggleArticle = function() {
-        $(this).toggleClass('hover');
-    };
-
-    TouchEffect.prototype.run = function() {
-        if (this.$articles.length > 0 && Modernizr.touch) {
-            this.initEvents();
-        }
-    };
-
-    module.exports = TouchEffect;
-})(jQuery);
+module.exports = TouchEffect;
