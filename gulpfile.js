0ead546106cc61dae1a2e9c23ef2c896ad8bde9b
Cyrille Perois <cperois@outlook.com>
2014-11-28 19:55:29 +0100

    Ajout de gulp-newer

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,31 @@ var gulp = require('gulp'),
     plumber = require('gulp-plumber'),
     sass = require('gulp-sass'),
     browserify = require('gulp-browserify'),
-    prefix = require('gulp-autoprefixer');
+    prefix = require('gulp-autoprefixer'),
+    newer = require('gulp-newer');
 
 gulp.task('styles', function() {
-    gulp.src('./assets/scss/*.scss')
+    gulp.src('assets/scss/*.scss')
         .pipe(plumber())
+        .pipe(newer('web/css'))
         .pipe(sass({
             outputStyle: 'compressed'
         }))
         .pipe(prefix('last 3 versions', '> 1%'))
-        .pipe(gulp.dest('./web/css'));
+        .pipe(gulp.dest('web/css'));
 });
 
 gulp.task('js', function() {
-    gulp.src('./assets/js/app.js')
+    gulp.src('assets/js/app.js')
         .pipe(plumber())
+        .pipe(newer('web/js/app.js'))
         .pipe(browserify())
-        .pipe(gulp.dest('./web/js'));
+        .pipe(gulp.dest('web/js'));
 });
 
 gulp.task('compile', ['styles', 'js']);
 
 gulp.task('watch', ['compile'], function() {
-    gulp.watch('./assets/scss/**/*.scss', ['styles']);
-    gulp.watch('./assets/js/**/*.js', ['js']);
+    gulp.watch('assets/scss/**/*.scss', ['styles']);
+    gulp.watch('assets/js/**/*.js', ['js']);
 });
