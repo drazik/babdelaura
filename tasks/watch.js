06e27c8e10b93538aaf840494524ebae8c41bd37
Cyrille Perois <cperois@outlook.com>
2015-10-04 17:41:27 +0200

    js -> scripts

--- tasks/watch.js
+++ tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 
 module.exports = function() {
     gulp.watch('assets/css/**/*.css', ['styles']);
-    gulp.watch('assets/js/**/*.js', ['js']);
+    gulp.watch('assets/js/**/*.js', ['scripts']);
     gulp.watch('assets/images/**/*.jpg', ['jpg']);
     gulp.watch('assets/images/**/*.png', ['png']);
     gulp.watch('assets/fonts/**/*.ttf', ['fonts']);
