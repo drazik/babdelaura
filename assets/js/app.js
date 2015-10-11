ecbb32440e04b3533d94b103ee6d69fcc597d210
Cyrille Perois <cperois@outlook.com>
2015-10-11 22:07:49 +0200

    DOM de PhotoSwipe, JS et CSS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,9 @@
 'use strict';
 
 var jQuery = require('jquery');
+var PhotoSwipe = require('photoswipe');
+var PhotoSwipeUiDefault = require('photoswipe/dist/photoswipe-ui-default');
+
 window.jQuery = jQuery;
 window.$ = jQuery;
 
@@ -13,3 +16,25 @@ mainNav.run();
 
 var touchEffect = new TouchEffect();
 touchEffect.run();
+
+var pswpElements = document.querySelectorAll('.pswp');
+
+var photoSwipeItems = [
+    {
+        src: 'https://placekitten.com/1200/1000',
+        w: 1200,
+        h: 1000
+    },
+    {
+        src: 'https://placekitten.com/1200/900',
+        w: 1200,
+        h: 900
+    }
+];
+
+var options = {
+    index: 0
+};
+
+var gallery = new PhotoSwipe(pswpElements[0], PhotoSwipeUiDefault, photoSwipeItems, options);
+gallery.init();
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -13,7 +13,7 @@ module.exports = function() {
     bundledStream
         .pipe(source('app.js'))
         .pipe(buffer())
-        .pipe(uglify())
+        // .pipe(uglify())
         .on('error', gutil.log)
         .pipe(gulp.dest('web/js'));
 
