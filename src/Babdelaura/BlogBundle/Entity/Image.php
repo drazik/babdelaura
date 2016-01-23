486d114bcff07d81c4353f7ffcc0ee2e98daa6c2
Cyrille Perois <cperois@outlook.com>
2016-01-23 15:33:23 +0100

    Modifications de l'image avant persist / persist de la taille de l'image / gestion d'erreurs

--- src/Babdelaura/BlogBundle/Entity/Image.php
+++ src/Babdelaura/BlogBundle/Entity/Image.php
@@ -255,4 +255,52 @@ class Image
     {
         return $this->articles;
     }
+
+    /**
+     * Set width
+     *
+     * @param integer $width
+     *
+     * @return Image
+     */
+    public function setWidth($width)
+    {
+        $this->width = $width;
+
+        return $this;
+    }
+
+    /**
+     * Get width
+     *
+     * @return integer
+     */
+    public function getWidth()
+    {
+        return $this->width;
+    }
+
+    /**
+     * Set height
+     *
+     * @param integer $height
+     *
+     * @return Image
+     */
+    public function setHeight($height)
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    /**
+     * Get height
+     *
+     * @return integer
+     */
+    public function getHeight()
+    {
+        return $this->height;
+    }
 }
