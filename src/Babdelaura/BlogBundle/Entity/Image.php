59645272542eec206ed12faabfd1c3704da786f8
laura-v <laura.vandeville@hotmail.fr>
2016-07-24 20:41:39 +0200

    Hotfix image

--- src/Babdelaura/BlogBundle/Entity/Image.php
+++ src/Babdelaura/BlogBundle/Entity/Image.php
@@ -55,13 +55,6 @@ class Image
      */
     private $height;
 
-    /**
-     * @var boolean
-     *
-     * @ORM\Column(name="watermark", type="boolean")
-     */
-    private $watermark;
-
     private $file;
 
     private $tempFilename;
@@ -344,29 +337,5 @@ class Image
     public function getHeight()
     {
         return $this->height;
-    }
-
-    /**
-     * Set watermark
-     *
-     * @param boolean $watermark
-     *
-     * @return Image
-     */
-    public function setWatermark($watermark)
-    {
-        $this->watermark = $watermark;
-
-        return $this;
-    }
-
-    /**
-     * Get watermark
-     *
-     * @return boolean
-     */
-    public function getWatermark()
-    {
-        return $this->watermark;
     }
 }
