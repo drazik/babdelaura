8fddd0f148ec173b5d689fe4f75eada3b3516a64
Cyrille Perois <cperois@outlook.com>
2016-07-28 22:40:55 +0200

    #51 prise en compte des modifications de Security de la version 2.6

--- src/Babdelaura/UserBundle/Controller/SecurityController.php
+++ src/Babdelaura/UserBundle/Controller/SecurityController.php
@@ -3,24 +3,18 @@
 namespace Babdelaura\UserBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        $request = $this->getRequest();
-        $session = $request->getSession();
-        // get the login error if there is one
-        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
-            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
-        } else {
-            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
-            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
-        }
+        $authenticationUtils = $this->get('security.authentication_utils');
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('BabdelauraUserBundle:Security:login.html.twig', array(
-            // last username entered by the user
-            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'last_username' => $lastUsername,
             'error'         => $error,
         ));
     }
