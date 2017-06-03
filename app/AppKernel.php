79244485e05b1d4226643be6c91bfe7862a5b515
Cyrille Perois <cperois@outlook.com>
2017-06-03 16:48:49 +0200

    install kachkaev/KachkaevAssetsVersionBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             //new Oh\InstagramBundle\OhInstagramBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
+            new Kachkaev\AssetsVersionBundle\KachkaevAssetsVersionBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
