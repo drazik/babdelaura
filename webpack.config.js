91c68e9bd070db63bac87e782143c53f2d77eedf
Cyrille Perois <cperois@outlook.com>
2017-06-18 22:07:54 +0200

    utilisation de imagemin-loader

--- webpack.config.js
+++ webpack.config.js
@@ -7,75 +7,96 @@ const webpack = require("webpack");
 const production = process.env.NODE_ENV === "production";
 
 module.exports = {
-    entry: {
-        app: "./assets/js/app.js",
-        admin: "./assets/js/admin.js",
-    },
-    output: {
-        path: path.resolve(__dirname, "web", "assets"),
-        publicPath: "/assets/",
-        filename: "[name].js",
-    },
-    module: {
-        rules: [
+  entry: {
+    app: "./assets/js/app.js",
+    admin: "./assets/js/admin.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "web", "assets"),
+    publicPath: "/assets/",
+    filename: "[name].js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["env"],
+          plugins: ["transform-object-rest-spread"]
+        },
+        include: path.resolve(__dirname, "assets", "js")
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
             {
-                test: /\.js$/,
-                loader: "babel-loader",
-                options: {
-                    presets: [
-                        "env",
-                    ],
-                    plugins: [
-                        "transform-object-rest-spread",
-                    ],
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                minimize: production,
+                sourceMap: !production
+              }
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: !production,
+                plugins: () => [
+                  require("postcss-import")(),
+                  require("postcss-url")(),
+                  require("postcss-custom-properties")(),
+                  require("postcss-custom-media")
+                ]
+              }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(svg|png|gif|jpg)$/,
+        use: [
+          { loader: "file-loader" },
+          {
+            loader: "imagemin-loader",
+            options: {
+              plugins: [
+                {
+                  use: require("imagemin-pngquant"),
+                  options: {}
                 },
-                include: path.resolve(__dirname, "assets", "js"),
-            },
-            {
-                test: /\.css$/,
-                use: ExtractTextPlugin.extract({
-                    fallback: "style-loader",
-                    use: [
-                        {
-                            loader: "css-loader",
-                            options: {
-                                importLoaders: 1,
-                                minimize: production,
-                                sourceMap: !production,
-                            },
-                        },
-                        {
-                            loader: "postcss-loader",
-                            options: {
-                                sourceMap: !production,
-                                plugins: () => [
-                                    require("postcss-import")(),
-                                    require("postcss-url")(),
-                                    require("postcss-custom-properties")(),
-                                    require("postcss-custom-media"),
-                                ],
-                            },
-                        },
-                    ],
-                }),
-            },
-            {
-                test: /\.(svg|png|gif|jpg)$/,
-                use: "file-loader",
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf)/,
-                use: "file-loader",
-            },
-        ],
-    },
-    plugins: [
-        new webpack.EnvironmentPlugin({
-            NODE_ENV: "development",
-        }),
-        new ExtractTextPlugin({
-            filename: "css/[name].css",
-            disable: !production,
-        }),
-    ],
+                {
+                  use: require("imagemin-mozjpeg"),
+                  options: {}
+                },
+                {
+                  use: require("imagemin-gifsicle"),
+                  options: {}
+                },
+                {
+                  use: require("imagemin-svgo"),
+                  options: {}
+                }
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)/,
+        use: "file-loader"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development"
+    }),
+    new ExtractTextPlugin({
+      filename: "css/[name].css",
+      disable: !production
+    })
+  ]
 };
