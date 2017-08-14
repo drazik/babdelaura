2122afd4faba8a80678dd3896b4a2d3864e2736e
laura-v <laura.vandeville@hotmail.fr>
2017-08-14 15:02:21 +0200

    création du bundle admin

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
             new EWZ\Bundle\RecaptchaBundle\EWZRecaptchaBundle(),
             new Kachkaev\AssetsVersionBundle\KachkaevAssetsVersionBundle(),
+            new Babdelaura\AdminBundle\BabdelauraAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'])) {
